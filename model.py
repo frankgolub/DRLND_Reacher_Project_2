0158cb705976f5f04366e0781a71e6dd1feebbd1
Frank Golub <frankgolub@gmail.com>
2019-05-07 22:18:41 -0400

    feat: BatchNorm1d adds to both actor and critic models

--- model.py
+++ model.py
@@ -25,7 +25,9 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc_units)
+        self.bn1 = nn.BatchNorm1d(fc_units)
         self.fc2 = nn.Linear(fc_units, action_size)
+        self.bn2 = nn.BatchNorm1d(action_size)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -34,8 +36,11 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.relu(self.fc1(state))
-        return F.tanh(self.fc2(x))
+        #x = F.relu(self.fc1(state))
+        x = self.fc1(state)
+        x = self.bn1(x)
+        x = F.relu(x)
+        return torch.tanh(self.bn2(self.fc2(x)))
 
 
 class Critic(nn.Module):
@@ -54,8 +59,11 @@ class Critic(nn.Module):
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.bn1 = nn.BatchNorm1d(fcs1_units)
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.bn2 = nn.BatchNorm1d(fc2_units)
         self.fc3 = nn.Linear(fc2_units, fc3_units)
+        self.bn3 = nn.BatchNorm1d(fc3_units)
         self.fc4 = nn.Linear(fc3_units, 1)
         self.reset_parameters()
 
@@ -67,8 +75,8 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.leaky_relu(self.fcs1(state))
+        xs = F.leaky_relu(self.bn1(self.fcs1(state)))
         x = torch.cat((xs, action), dim=1)
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        x = F.leaky_relu(self.bn2(self.fc2(x)))
+        x = F.leaky_relu(self.bn3(self.fc3(x)))
         return self.fc4(x)
